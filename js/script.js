615a133c523bec999a37cda984f854514c7d806e
Vyacheslav <stormfall228@gmai.com>
2025-07-23 18:49:37 +0500

    final

--- js/script.js
+++ js/script.js
@@ -22,3 +22,50 @@ popup.addEventListener('click', () => {
         popup.style.display = ''
     }, 600)
 })
+
+
+const burgerBtn = document.querySelector('.burger')
+const closeBurger = document.querySelector('.close__burger')
+const mainMenu = document.querySelector('.main__menu')
+const linkMenu = document.querySelectorAll('.link__menu')
+
+burgerBtn.addEventListener('click', () => {
+    mainMenu.style.display = 'block'
+    setTimeout(() => {
+        mainMenu.style.opacity = '1'
+    },750)
+    setTimeout(() => {
+        mainMenu.style.transform = 'translateY(0)'
+    },800)
+})
+
+closeBurger.addEventListener('click', () => {
+    mainMenu.style.transform = ''
+    mainMenu.style.opacity = ''
+    setTimeout(() => {
+        mainMenu.style.display = ''
+    },600)
+})
+
+for(let linksMenu of linkMenu){
+    linksMenu.addEventListener('click', () => {
+        mainMenu.style.transform = ''
+        mainMenu.style.opacity = ''
+        setTimeout(() => {
+            mainMenu.style.display = ''
+        },600)
+    })
+}
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
